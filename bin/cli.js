064ed87025f5a0b74c36991ef106f4eec28a14c0
Kaioru <keifuurawrs@gmail.com>
2017-10-12 20:51:19 +0800

    Add cli

--- bin/cli.js
+++ bin/cli.js
@@ -1,1 +1,21 @@
 var memefy = require('../lib/memefy')
+var commander = require('commander')
+var pkg = require('../package.json')
+
+commander
+  .version(pkg.version)
+
+commander.command('alternating').action(text => console.log(memefy.alternating(text)))
+commander.command('alternating2').action(text => console.log(memefy.alternating2(text)))
+commander.command('stoz').action(text => console.log(memefy.stoz(text)))
+
+commander.command('ccfy').action(text => console.log(memefy.ccfy(text)))
+commander.command('escalate').action(text => console.log(memefy.escalate(text)))
+commander.command('leet').action(text => console.log(memefy.leet(text)))
+commander.command('spaceout').action(text => console.log(memefy.spaceout(text)))
+
+if (!process.argv.slice(2).length) {
+  commander.outputHelp();
+}
+
+commander.parse(process.argv);
