7baa60230492c34ca9b1bb881731e0f6b5827d25
Kaioru <keifuurawrs@gmail.com>
2017-10-15 13:56:17 +0800

    Add renaming of index.js to memefy.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,18 @@
 const gulp = require('gulp');
 const browserify = require('gulp-browserify');
-const pkg = require('./package.json')
+const rename = require("gulp-rename");
+const pkg = require('./package.json');
 
 gulp.task('build', () =>
   gulp.src(pkg.main)
     .pipe(browserify({
-      insertGlobals : true,
-      debug : !gulp.env.production
-    }))
-    .pipe(gulp.dest('dist'))
+    insertGlobals: true,
+    debug: !gulp.env.production
+  }))
+  .pipe(rename(function(path) {
+    path.basename = pkg.name;
+  }))
+  .pipe(gulp.dest('dist'))
 );
 
 gulp.task('default', ['build']);
