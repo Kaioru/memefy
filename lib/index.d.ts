f3710ba6e7e0ab158340011ba5e4df0b7ea1e4d6
PishangCode <m.akmalhakim95@gmail.com>
2017-10-16 14:47:15 +0800

    bundled typescript definition

---
+++ lib/index.d.ts
@@ -0,0 +1,11 @@
+declare namespace Memefy {
+    function alternating(word: string): void;
+    function alternating2(word: string): void;
+    function stoz(word: string): void;
+    function ccfy(word: string): void;
+    function escalate(word: string): void;
+    function leet(word: string): void;
+    function spaceout(word: string): void;
+}
+
+export = Memefy;
