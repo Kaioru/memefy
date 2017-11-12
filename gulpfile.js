f8bac4fdb99858cfc462e6cd1de2943ae19248a8
Kaioru <keifuurawrs@gmail.com>
2017-11-12 20:19:05 +0800

    Add uglifying of distributables

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const gulp = require('gulp');
 const browserify = require('browserify');
 const sourcemaps = require('gulp-sourcemaps');
 const rename = require("gulp-rename");
+const uglify = require('gulp-uglify-es').default;
 const source = require('vinyl-source-stream');
 const buffer = require('vinyl-buffer');
 const pkg = require('./package.json');
@@ -19,6 +20,7 @@ gulp.task('build', () => {
     .pipe(sourcemaps.init({
       loadMaps: true
     }))
+    .pipe(uglify())
     .pipe(rename(function(path) {
       path.basename = pkg.name;
     }))
